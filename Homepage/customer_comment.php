7aab6aa64a1aa03f54e1af1fce733539ca5cf1fa
BRIAN LEE CHONG MING <1211205395@student.mmu.edu.my>
2023-06-16 01:47:11 +0800

    NEW

---
+++ Homepage/customer_comment.php
@@ -0,0 +1,27 @@
+<?php
+
+
+include("dataconnection.php");
+
+if(isset($_POST["btn"]))
+{
+    $name = $_POST["username"];
+    $email = $_POST["useremail"];
+    $cmt = $_POST["comment"];
+
+    $sql = mysqli_query($connect,"INSERT INTO Comment(name_com,email_com,text_com) VALUES ('$name','$email','$cmt') ");  //Allow user to make query in database
+                                    //Attribute (Fname,....from table database)    //VALUES (from Declaration)
+
+    if($sql)
+    {
+        ?>
+        <script>
+            alert("Record Saved!"); //BASIC WAYS FOR CHECKING
+        </script>
+        <?php
+    }
+
+
+}
+
+?>
