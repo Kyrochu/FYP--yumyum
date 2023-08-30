ac39cb50d388a6e84912b56dbee7a7527accb788
JAMESLEWMINGREN <1211205394@student.mmu.edu.my>
2023-08-30 09:26:20 +0800

    new login

---
+++ login/login.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="login.css">
+    <style>
+        /* 添加这个样式，以实现切换效果 */
+        .right-panel-active .overlay-left {
+            transform: translateX(-50%);
+        }
+    
+        .right-panel-active .overlay-right {
+            transform: translateX(0);
+        }
+    </style>
+</head>
+<body>
+    <div class="container" id="container">
+        <div class="form-container sign-up-container">
+            <form action="#">
+                <h1>Sign Up</h1>
+                <div class="social-container">
+                    <a href="#" class="social-f"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social-g"><i class="fab fa-google-plus-g"></i></a>
+
+                </div>
+                <span>or use your email for registration</span>
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Contact Number" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <input type="password" placeholder="Comfirm Password" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+        <div class="form-container sign-in-container">
+            <form action="#">
+                <h1>Sign in</h1>
+                <div class="social-container">
+                    <a href="#" class="social-f"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#" class="social-g"><i class="fab fa-google-plus-g"></i></a>
+                </div>
+                <span>or use your account</span>
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <div class="forgot-line">
+                    <a href="forgot-password.php">Forgot your password?</a>
+                </div>
+                
+                <button>Sign In</button>
+            </form>
+        </div>
+        <div class="overlay-container">
+            <div class="overlay">
+                <div class="overlay" style="background-image: url('chef.jpg');"></div>
+                <div class="overlay-panel overlay-left">
+                    <h1>Welcome Back!</h1>
+                    <p>To keep connected with us please login with your personal info</p>
+                    <button class="ghost" id="signIn">Sign In</button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Enter your personal details and start journey with us</p>
+                    <button class="ghost" id="signUp">Sign Up</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    
+</body>
+</html>
+
+<script>
+
+        document.addEventListener("DOMContentLoaded", function () {
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const container = document.getElementById('container');
+        const passwordInput = document.getElementById('passwordInput');
+        const eyeIcon = document.getElementById('eyeIcon');
+        
+        signUpButton.addEventListener('click', () => {
+            container.classList.add("right-panel-active");
+        });
+
+        signInButton.addEventListener('click', () => {
+            container.classList.remove("right-panel-active");
+        });
+
+        passwordInput.addEventListener('focus', () => {
+            if (passwordInput.value.length > 0) {
+                eyeIcon.style.opacity = '1';
+                eyeIcon.style.pointerEvents = 'auto';
+            }
+        });
+
+        passwordInput.addEventListener('blur', () => {
+            eyeIcon.style.opacity = '0';
+            eyeIcon.style.pointerEvents = 'none';
+        });
+
+        eyeIcon.addEventListener('click', () => {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                eyeIcon.innerHTML = '<i class="fas fa-eye-slash"></i>';
+            } else {
+                passwordInput.type = 'password';
+                eyeIcon.innerHTML = '<i class="fas fa-eye"></i>'; // Update this line
+            }
+        });
+    });
+</script>
---
+++ login/signup.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box}
+
+.container {
+	background-color: #fff;
+	border-radius: 10px;
+  	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+	position: relative;
+	overflow: hidden;
+	width: 1500px;
+	max-width: 100%;
+	min-height: 150px;
+	margin: 0 auto; /* Center the container horizontally */
+}
+
+.sign-up-container{
+
+    
+}
+
+form {
+	background-color: #FFFFFF;
+	display: flex;
+	flex-direction: column; /* Change to column layout for input fields */
+	align-items: center; /* Center items horizontally */
+	padding: 0 50px;
+	height: 100%;
+	text-align: center;
+}
+
+input {
+	background-color: #eee;
+	border: none;
+	padding: 12px 15px;
+	margin: 8px 0;
+	width: 60%;
+}
+
+hr {
+  border: 1px solid #f1f1f1;
+  margin-bottom: 25px;
+}
+
+/* Set a style for all buttons */
+button {
+  background-color: #04AA6D;
+  color: white;
+  padding: 14px 20px;
+  margin: 8px 0;
+  border: none;
+  cursor: pointer;
+  width: 100%;
+  opacity: 0.9;
+}
+
+button:hover {
+  opacity:1;
+}
+
+/* Float cancel and signup buttons and add an equal width */
+.signupbtn {
+  float: right;
+  width: 20%;
+}
+
+/* Add padding to container elements */
+.container {
+  padding: 50px;
+  width: 1000px;
+}
+
+/* Center the "Sign Up" text */
+h1 {
+  text-align: center;
+}
+
+p {
+  text-align: center;
+}
+
+</style>
+<body>
+
+<div class="container">
+  <h1>Sign Up</h1>
+  <p>Please fill in this form to create an account.</p>
+  <hr>
+  <div class="container" id="container">
+    <div class="form-container sign-up-container">
+      <input type="text" placeholder="Name" id="name" />
+      <input type="text" placeholder="Contact Number" id="contact" />
+      <input type="email" placeholder="Email" id="email" />
+      <input type="password" placeholder="Password" id="password" />
+      <input type="password" placeholder="Confirm Password" id="confirmPassword" />
+    </div>
+  </div>
+  <div class="clearfix">
+    <button type="submit" class="signupbtn" onclick="validateForm()">Sign Up</button>
+  </div>
+</div>
+
+<script>
+function validateForm() {
+  const name = document.getElementById("name").value;
+  const contact = document.getElementById("contact").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+
+  if (name === "" || contact === "" || email === "" || password === "" || confirmPassword === "") {
+    alert("Please fill in all fields.");
+    return false;
+  }
+
+  // Additional validation logic can be added here
+
+  // If all fields are filled, proceed with form submission
+  // Your form submission code goes here
+}
+</script>
+
+</body>
+</html>
