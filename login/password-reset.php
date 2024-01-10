db8d3b6dc21a5d93362f8fd13223c9a3630fec2f
CHUWEIWQANG <1211205392@student.mmu.edu.my>
2024-01-10 23:16:28 +0800

    s

---
+++ login/password-reset.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Forget Password</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">   
+    <meta charset="utf-8">
+</head>
+<body>
+  
+  <h1>Forget Password</h1>
+  <form method="post" action="send-password-reset.php"></form>
+
+    <label for="email">email</label>
+    <input type="email" name="email" id="email">
+
+  <button>Send</button>
+
+</body>
+</html>
