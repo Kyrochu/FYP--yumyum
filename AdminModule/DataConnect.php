ac22d8461ed4de6cd3510de0cfed1786f62b6b3a
BRIAN LEE CHONG MING <1211205395@student.mmu.edu.my>
2024-01-10 23:56:31 +0800

    N

--- AdminModule/DataConnect.php
+++ AdminModule/DataConnect.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-$connect=mysqli_connect("localhost","root","","yumyum");
+$connect=mysqli_connect("localhost","root"," ","yumyum");
 
 // 1. Server name 2. Username 3. Password 4. Database Name
 
