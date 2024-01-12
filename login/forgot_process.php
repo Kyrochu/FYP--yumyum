9db2aeb7c00b28decb36e5a71e99a1c82b64efa0
JAMESLEWMINGREN <1211205394@student.mmu.edu.my>
2024-01-12 16:32:00 +0800

    weeeeekkkk11111

--- login/forgot_process.php
+++ login/forgot_process.php
@@ -8,14 +8,14 @@ do {
 } while (ctype_alpha($token[0]));
 
 
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'vendor/phpmailer/phpmailer/src/PHPMailer.php'; 
-    require 'vendor/phpmailer/phpmailer/src/Exception.php';
-    require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+    require '../vendor/phpmailer/phpmailer/src/PHPMailer.php'; 
+    require '../vendor/phpmailer/phpmailer/src/Exception.php';
+    require '../vendor/phpmailer/phpmailer/src/SMTP.php';
 
     $mail = new PHPMailer(true);
 
