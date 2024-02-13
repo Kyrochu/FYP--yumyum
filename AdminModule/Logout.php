68bc45971451169400737383d20649043d2f50fc
BRIAN LEE CHONG MING <1211205395@student.mmu.edu.my>
2024-02-14 00:34:11 +0800

    x

--- AdminModule/Logout.php
+++ AdminModule/Logout.php
@@ -6,5 +6,5 @@ $_SESSION = [];
 session_unset();    // Remove all session variables(session data) without deleting
 session_destroy();   // Delete session data 
 header("location:AdminLogin.php"); // Redirect to this page
-
+ 
 ?>
